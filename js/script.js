8bb1f25390ec2ab0669f56727c14e6bef0b8baca
Valerio <valeriodevivo90@gmail.com>
2020-12-22 16:58:16 +0100

    Finish with bonus

--- js/script.js
+++ js/script.js
@@ -1,55 +1,164 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
 
 // Il computer genera 16 numeri random senza doppioni 
 
-var numeriComputerRandom = [];
+// var numeriComputerRandom = [];
 
-while (numeriComputerRandom.length < 16) {
+// while (numeriComputerRandom.length < 16) {
 
-    var numeri = randomNumbers(1,100);
+//     var numeri = randomNumbers(1,100);
 
-    if (numeriComputerRandom.includes(numeri) == false) {
-        numeriComputerRandom.push(numeri);
-    }
-}
+//     if (numeriComputerRandom.includes(numeri) == false) {
+//         numeriComputerRandom.push(numeri);
+//     }
+// }
 
 
-// Si chiede all utente di inserire i numeri uno alla volta 
+// SI CHIEDE LA DIFFICOLTà ALL'UTENTE
+
+var difficolta = prompt('Scegli la difficoltà: 1 / 2 / 3');
 
 var numeriUtente = [];
 var punteggio = 0;
 
+switch (difficolta) {
+    case "1":
 
-for( var i = 1 ; i < 85 ; i++) {
-    var numeroUtente = parseInt(prompt('Inserisci un numero e sfida la sorte'));
+        var numeriComputerRandom = [];
 
-    if ( numeroUtente > 100) {
-        alert('puoi inserire un numero fino a 100! ');
-        numeroUtente;
-        i--;
-    } else if ( numeriComputerRandom.includes(numeroUtente) == true) {
-        alert('Hai perso!');
-        document.getElementById('numeri-computer').innerHTML= numeriComputerRandom;
-        i = 84;
-    } else if (numeriUtente.includes(numeroUtente)) {
-        alert('Hai già inserito questo numero!');
-        i--;
-    } else {
-        numeriUtente.push(numeroUtente);
-        punteggio++;
-    }
+        while (numeriComputerRandom.length < 16) {
+        
+            var numeri = randomNumbers(1,100);
+        
+            if (numeriComputerRandom.includes(numeri) == false) {
+                numeriComputerRandom.push(numeri);
+            }
+        }
 
-    console.log('punteggio' + punteggio);
-}
+        for( var i = 1 ; i < 85 ; i++) {
+            var numeroUtente = parseInt(prompt('Inserisci un numero e sfida la sorte'));
+        
+            if ( numeroUtente > 100 || numeroUtente < 1 || numeroUtente == 'NaN') {
+                alert('puoi inserire un numero da 1 a 100! ');
+                numeroUtente;
+                i--;
+            } else if ( numeriComputerRandom.includes(numeroUtente) == true) {
+                alert('Hai perso!');
+                document.getElementById('numeri-computer').innerHTML= 'Numeri Bomba ' + numeriComputerRandom;
+                i = 84;
+            } else if (numeriUtente.includes(numeroUtente)) {
+                alert('Hai già inserito questo numero!');
+                i--;
+            } else {
+                numeriUtente.push(numeroUtente);
+                punteggio++;
+            }
+        }
+    break;
+    case "2":
 
-console.log(punteggio);
+        var numeriComputerRandom = [];
+
+        while (numeriComputerRandom.length < 16) {
+        
+            var numeri = randomNumbers(1,80);
+        
+            if (numeriComputerRandom.includes(numeri) == false) {
+                numeriComputerRandom.push(numeri);
+            }
+        }
+
+
+        for( var i = 1 ; i < 65 ; i++) {
+            var numeroUtente = parseInt(prompt('Inserisci un numero e sfida la sorte'));
+        
+            if ( numeroUtente > 80 || numeroUtente < 1 || numeroUtente == 'NaN') {
+                alert('puoi inserire un numero da 1 a 80! ');
+                numeroUtente;
+                i--;
+            } else if ( numeriComputerRandom.includes(numeroUtente) == true) {
+                alert('Hai perso!');
+                document.getElementById('numeri-computer').innerHTML= 'Numeri Bomba ' + numeriComputerRandom;
+                i = 64;
+            } else if (numeriUtente.includes(numeroUtente)) {
+                alert('Hai già inserito questo numero!');
+                i--;
+            } else {
+                numeriUtente.push(numeroUtente);
+                punteggio++;
+            }
+        }
+    break;
+    case "3":
+
+        var numeriComputerRandom = [];
+
+        while (numeriComputerRandom.length < 16) {
+        
+            var numeri = randomNumbers(1,50);
+        
+            if (numeriComputerRandom.includes(numeri) == false) {
+                numeriComputerRandom.push(numeri);
+            }
+        }
+
+
+        for( var i = 1 ; i < 35 ; i++) {
+            var numeroUtente = parseInt(prompt('Inserisci un numero e sfida la sorte'));
+        
+            if ( numeroUtente > 50 || numeroUtente < 1 || numeroUtente == 'NaN') {
+                alert('puoi inserire un numero da 1 a 50! ');
+                numeroUtente;
+                i--;
+            } else if ( numeriComputerRandom.includes(numeroUtente) == true) {
+                alert('Hai perso!');
+                document.getElementById('numeri-computer').innerHTML= 'Numeri Bomba ' + numeriComputerRandom;
+                i = 34;
+            } else if (numeriUtente.includes(numeroUtente)) {
+                alert('Hai già inserito questo numero!');
+                i--;
+            } else {
+                numeriUtente.push(numeroUtente);
+                punteggio++;
+            }
+        }
+    break;
+    
+    default:
+        difficolta = alert('Scrivi 1 o 2 o 3 in base alla difficoltà che vuoi, ricarica la pagina per giocare');
+} 
+
+
+// Si chiede all utente di inserire i numeri uno alla volta 
+
+
+
+
+// for( var i = 1 ; i < 85 ; i++) {
+//     var numeroUtente = parseInt(prompt('Inserisci un numero e sfida la sorte'));
+
+//     if ( numeroUtente > 100 || numeroUtente < 1 || numeroUtente == 'NaN') {
+//         alert('puoi inserire un numero da 1 a 100! ');
+//         numeroUtente;
+//         i--;
+//     } else if ( numeriComputerRandom.includes(numeroUtente) == true) {
+//         alert('Hai perso!');
+//         document.getElementById('numeri-computer').innerHTML= 'Numeri Bomba ' + numeriComputerRandom;
+//         i = 84;
+//     } else if (numeriUtente.includes(numeroUtente)) {
+//         alert('Hai già inserito questo numero!');
+//         i--;
+//     } else {
+//         numeriUtente.push(numeroUtente);
+//         punteggio++;
+//     }
+// }
+
+
+
+document.getElementById('numeri-utente').innerHTML = 'Questi sono i numeri che hai indovinato: ' + numeriUtente;
+
+
+document.getElementById('punteggio').innerHTML = 'Hai fatto ' + punteggio + ' punti!';
 
 
 
